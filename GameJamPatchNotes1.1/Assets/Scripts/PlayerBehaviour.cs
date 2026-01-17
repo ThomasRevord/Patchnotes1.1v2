e9b8b4c45aeed039bdd7950300764e124e597afe
ThomasRevord <94570592+ThomasRevord@users.noreply.github.com>
2026-01-17 11:56:50 -0600

    player movement

---
+++ GameJamPatchNotes1.1/Assets/Scripts/PlayerBehaviour.cs
@@ -0,0 +1,38 @@
+using UnityEngine;
+
+public class PlayerBehaviour : MonoBehaviour
+{
+    //variables
+    public float speed;
+    public Rigidbody rigid;
+    public Vector3 rotation;
+    public float rotateSpeed;
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        //movement (using old input system)
+        if (Input.GetKey(KeyCode.UpArrow))
+        {
+            transform.Translate(Vector3.forward * Time.deltaTime * speed);
+        }
+        else if (Input.GetKey(KeyCode.DownArrow))
+        {
+            transform.Translate(Vector3.back * Time.deltaTime * speed);
+        }
+        if (Input.GetKey(KeyCode.LeftArrow))
+        {
+            transform.Rotate(rotation * rotateSpeed * Time.deltaTime);
+        }
+
+        if (Input.GetKey(KeyCode.RightArrow))
+        {
+            transform.Rotate(rotation * rotateSpeed * Time.deltaTime * -1);
+        }
+    }
+}
