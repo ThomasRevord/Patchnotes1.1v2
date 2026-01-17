ea5ed96f29d068208e9b21eeee1282ec1274f24a
ThomasRevord <94570592+ThomasRevord@users.noreply.github.com>
2026-01-17 16:50:37 -0600

    inventory system

---
+++ GameJamPatchNotes1.1/Assets/Scripts/Item.cs
@@ -0,0 +1,21 @@
+using UnityEngine;
+
+public class Item : MonoBehaviour
+{
+    //variables
+    public string itemName;
+    public Sprite icon;
+    public string GetItem()
+    {
+        return itemName;
+    }
+    //methods to show wheter or not Item is interactable
+    public void showChild()
+    { 
+    
+    }
+    public void hideChild()
+    { 
+    
+    }
+}
--- GameJamPatchNotes1.1/Assets/Scripts/PlayerBehaviour.cs
+++ GameJamPatchNotes1.1/Assets/Scripts/PlayerBehaviour.cs
@@ -1,4 +1,6 @@
 using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
 
 public class PlayerBehaviour : MonoBehaviour
 {
@@ -7,6 +9,9 @@ public class PlayerBehaviour : MonoBehaviour
     public Rigidbody rigid;
     public Vector3 rotation;
     public float rotateSpeed;
+    //inventory system
+    public List<string> items;
+    public Item stored = null;
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
@@ -34,5 +39,31 @@ public class PlayerBehaviour : MonoBehaviour
         {
             transform.Rotate(rotation * rotateSpeed * Time.deltaTime * -1);
         }
+        if (Input.GetKeyDown(KeyCode.E) && stored != null)
+        {
+            //add to inventory
+            items.Add(stored.GetItem());
+        }
+    }
+    //when hitting a trigger
+    private void OnTriggerEnter(Collider other)
+    {
+        Debug.Log("HIT");
+        //and trigger is an item
+        if (other.gameObject.tag.Equals("Item"))
+        {
+            stored = other.gameObject.GetComponent<Item>();
+            if (Input.GetKeyDown(KeyCode.E))
+            {
+                //add to inventory
+                items.Add(stored.GetItem());
+            }
+            
+        }
+    }
+
+    private void OnTriggerExit(Collider other)
+    {
+        stored = null;
     }
 }
